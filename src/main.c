c5c430b11d4e6fe82efd52d0202cbbd904d337a1
Ihor Ryzhov <ihor.ryzhov2019@gmail.com>
2024-11-26 21:43:09 +0200

    WIP

--- src/main.c
+++ src/main.c
@@ -1,28 +1,9 @@
 #include <stdio.h>
-#include "input-output-device/input-output-device.h"
-
-int callback(
-    const void *input, void *output,
-    unsigned long frameCount,
-    const PaStreamCallbackTimeInfo* timeInfo,
-    PaStreamCallbackFlags statusFlags,
-    void *userData )
-{
-
-
-	return 0;
-}
+#include <pthread.h>
 
 int main(void)
 {
-	inputOutputDevice_initDevice(callback);
-
-	inputOutputDevice_startOutputDevice();
-
-	while(1)
-	{
-
-	}
+	pthread_self();
 
 	printf("Hello world");
 
--- src/toolchains/arm-toolchain.cmake
+++ src/toolchains/arm-toolchain.cmake
@@ -1,12 +1,14 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR ARM)
 
-set(CMAKE_C_COMPILER /home/ihor/Toolchains/gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-gcc)
-set(CMAKE_CXX_COMPILER /home/ihor/Toolchains/gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-g++)
+#set(CMAKE_C_COMPILER_WORKS 1)
+#set(CMAKE_CXX_COMPILER_WORKS 1)
 
-# set sysroot
-SET(CMAKE_SYSROOT /home/ihor/Toolchains/gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf/bin/../arm-linux-gnueabihf/libc)
-SET(CMAKE_FIND_ROOT_PATH /home/ihor/Toolchains/gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf/bin/../arm-linux-gnueabihf/libc)
+#set(CMAKE_C_COMPILER arm-none-linux-gnueabihf-gcc)
+#set(CMAKE_CXX_COMPILER arm-none-linux-gnueabihf-g++)
+
+#set(CMAKE_FIND_ROOT_PATH /home/ihor/workspace/sysroot)
+#SET(CMAKE_SYSROOT /home/ihor/workspace/sysroot)
 
 # search for programs in the build host directories
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
@@ -17,3 +19,5 @@ SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 SET(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -std=gnu11")
+
+#set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
