3e76ab2209a9c601525bfcfd00c0ce7c2c1f91cd
IhorRyzhov <ihor.ryzhov2019@gmail.com>
2024-11-30 23:59:29 +0200

    Include thirg party libraries in the build configuration

--- src/main.c
+++ src/main.c
@@ -1,11 +1,19 @@
+/** 
+ * @file main.c
+ * @brief Play a sine wave for several seconds, pausing in the middle.
+*/
 #include <stdio.h>
-#include <pthread.h>
+#include <math.h>
+#include "portaudio/include/portaudio.h"
+#include "liquid-dsp/include/liquid.h"
+
+static float complex cfP_buff1[2048];
+
+static float complex cfP_buff2[2048];
 
 int main(void)
 {
-	pthread_self();
-
-	printf("Hello world");
+    fftplan objPL_pbw = fft_create_plan(2048, cfP_buff1, cfP_buff2, LIQUID_FFT_BACKWARD, 0); //static
 
 	return 0;
 }
