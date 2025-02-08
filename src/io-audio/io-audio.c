d9e552253352d59f636b28443110c1c001ab4eda
Ihor Ryzhov <ihor.ryzhov2019@gmail.com>
2025-02-08 15:49:24 +0200

    Use the libsoundio library instead of the miniaudio library

--- src/io-audio/io-audio.c
+++ src/io-audio/io-audio.c
@@ -1,82 +1,33 @@
 #include "io-audio.h"
-
-#define MA_NO_DECODING
-#define MA_NO_ENCODING
-#define MA_NO_FLAC
-#define MA_NO_MP3
-#define MA_NO_WAV
-#define MA_NO_RESOURCE_MANAGER
-#define MA_NO_ENGINE
-#define MA_NO_GENERATION
-
-#define MINIAUDIO_IMPLEMENTATION
-#include "../miniaudio/miniaudio.h"
 #include "../libwave/include/wave.h"
+#include "../libsoundio/soundio/soundio.h"
 
-#define SAMPLE_RATE 48000
+#define SAMPLE_RATE 44100
 
-typedef struct MyDataSource
-{
-    ma_data_source_base obj_base;    
-} myDataSource_t;
-
-static ma_device obj_device;
-static myDataSource_t obj_dataSource;
 static WaveFile* objP_file = NULL;
 static WaveFile* objP_fileCapture = NULL;
 
-static void dataCallback(ma_device* objPL_device, void* pL_output, const void* pL_input, ma_uint32 u32L_frameCount)
-{
-    ma_data_source_read_pcm_frames(&obj_dataSource, pL_output, u32L_frameCount, NULL);
+//wave_write(objP_fileCapture, pL_input, u32L_frameCount);
+//wave_read(objP_file, pL_framesOut, u64L_frameCount);   
 
-    wave_write(objP_fileCapture, pL_input, u32L_frameCount);
+static void write_callback(struct SoundIoOutStream *outstream, int frame_count_min, int frame_count_max) 
+{
+    double float_sample_rate = outstream->sample_rate;
+    struct SoundIoChannelArea *areas;
+    int err;
+    int s32L_framesToWrite = frame_count_max;
+
+    err = soundio_outstream_begin_write(outstream, &areas, &s32L_framesToWrite);
+
+    wave_read(objP_file, areas->ptr, frame_count_max);
+    areas->step = 2;
+
+    err = soundio_outstream_end_write(outstream);
 }
 
-static ma_result myDataSourceRead(ma_data_source* objPL_dataSource, void* pL_framesOut, ma_uint64 u64L_frameCount, ma_uint64* u64PL_framesRead)
-{
-    wave_read(objP_file, pL_framesOut, u64L_frameCount);    
-}
-
-static ma_result myDataSourceSeek(ma_data_source* pDataSource, ma_uint64 frameIndex)
-{
-    return MA_NOT_IMPLEMENTED;
-}
-
-static ma_result myDataSourceGetDataFormat(
-    ma_data_source* objPL_dataSource, 
-    ma_format* objPL_format, 
-    ma_uint32* u32PL_channels, 
-    ma_uint32* u32PL_sampleRate, 
-    ma_channel* objPL_channelMap, 
-    size_t u32L_channelMapCap)
-{
-    *objPL_format = ma_format_s16;
-    *u32PL_channels = 2;
-    *u32PL_sampleRate = SAMPLE_RATE;
-
-    return MA_SUCCESS;
-}
-
-static ma_result myDataSourceGetCursor(ma_data_source* objPL_dataSource, ma_uint64* u64PL_cursor)
-{
-    *u64PL_cursor = 0;
-    return MA_NOT_IMPLEMENTED;
-}
-
-static ma_result myDataSourceGetLength(ma_data_source* objPL_dataSource, ma_uint64* u64PL_length)
-{
-    *u64PL_length = 0;
-    return MA_NOT_IMPLEMENTED;
-}
-
-static ma_data_source_vtable obj_myDataSourceVtable =
-{
-    myDataSourceRead,
-    myDataSourceSeek,
-    myDataSourceGetDataFormat,
-    myDataSourceGetCursor,
-    myDataSourceGetLength
-};
+static struct SoundIo *soundio;
+static struct SoundIoDevice *device;
+static struct SoundIoOutStream *outstream;
 
 void ioAudio_init(void)
 {
@@ -94,43 +45,42 @@ void ioAudio_init(void)
     wave_set_sample_rate(objP_fileCapture, SAMPLE_RATE);
     wave_set_valid_bits_per_sample(objP_fileCapture, 16);
     wave_set_sample_size(objP_fileCapture, 2);
+
+    soundio = soundio_create();
   
+    int err = soundio_connect_backend(soundio, SoundIoBackendAlsa);
 
-    ma_result eL_err = MA_SUCCESS;
+    soundio_flush_events(soundio);
 
-    ma_data_source_config objL_baseConfig;
-    objL_baseConfig = ma_data_source_config_init();
-    objL_baseConfig.vtable = &obj_myDataSourceVtable;
-    ma_data_source_init(&objL_baseConfig, &obj_dataSource.obj_base);
+    int selected_device_index = soundio_default_output_device_index(soundio);
+
+    device = soundio_get_output_device(soundio, selected_device_index);
+
+    outstream = soundio_outstream_create(device);
+
+
+    outstream->write_callback = write_callback;
+    outstream->underflow_callback = NULL;
+    outstream->software_latency = 0.1;
+    outstream->sample_rate = SAMPLE_RATE;
     
-    ma_device_config objL_deviceConfig;
-    objL_deviceConfig = ma_device_config_init(ma_device_type_duplex);
-    objL_deviceConfig.playback.format = ma_format_s16;
-    objL_deviceConfig.playback.channels = 2;
-    objL_deviceConfig.capture.format = ma_format_s16;
-    objL_deviceConfig.capture.channels = 2;
-    objL_deviceConfig.sampleRate = SAMPLE_RATE;
-    objL_deviceConfig.dataCallback = dataCallback;
-    objL_deviceConfig.pUserData = NULL;
-    objL_deviceConfig.periodSizeInFrames = 128; // frame buffer size
-
-    if (ma_device_init(NULL, &objL_deviceConfig, &obj_device) != MA_SUCCESS) 
+    if (soundio_device_supports_format(device, SoundIoFormatS16NE)) 
     {
-        return;
+        outstream->format = SoundIoFormatS16NE;
     }
 
-    if (ma_device_start(&obj_device) != MA_SUCCESS) 
-    {
-        ma_device_uninit(&obj_device);
-        return;
-    }
+    err = soundio_outstream_open(outstream);
+
+    err = soundio_outstream_start(outstream);
+
+
 }
 
 void ioAudio_deinit(void)
 {
-    ma_device_uninit(&obj_device);
-
-    ma_data_source_uninit(&obj_dataSource.obj_base);
+    soundio_outstream_destroy(outstream);
+    soundio_device_unref(device);
+    soundio_destroy(soundio);
 
     wave_close(objP_fileCapture);
 
